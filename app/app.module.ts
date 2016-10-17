e10352d2893e2d93ab5fd0ac6f49e7ce7577dd4a
Liam McCartney <liam.mccartney@gmail.com>
2016-10-17 14:48:29 -0400

    Welp, that's everything I guess

---
+++ app/app.module.ts
@@ -0,0 +1,40 @@
+import './rxjs-extensions';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService } from './in-memory-data.service';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        DashboardComponent,
+        HeroSearchComponent,
+        HeroDetailComponent
+    ],
+    providers: [ HeroService ],
+    bootstrap: [ AppComponent ]
+})
+
+export class AppModule { }
+
